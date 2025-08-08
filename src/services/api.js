6fc121ce49b58808b265b52ed10adfc1be64b6b6
kshitiz-28 <kshitiz.gwl2k@gmail.com>
2025-08-09 01:28:20 +0530

    Fix API base URL env variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Get API URL from environment variable (default to localhost for safety)
-const API_BASE =  process.env.REACT_APP_API_BASE;;
+const API_BASE = process.env.REACT_APP_API_URL;
 
 // ---------- USERS ----------
 export const getAllUsers = () => axios.get(`${API_BASE}/users`);
